b84cad95c895d3006413918cfc8af536c2cfa902
erjohny <75377407+erjohny@users.noreply.github.com>
2022-12-20 13:25:16 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import LoginScreen from "./src/views/screens/LoginScreen";
-// import RegistrationScreen from "./src/views/screens/RegistrationScreen";
-// import HomeScreen from "./src/views/screens/HomeScreen";
-
-
-// const Stack = createNativeStackNavigator();
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator screenOptions={{ headerShown: false }}>
-//         <Stack.Screen name="RegistrationScreen" component={RegistrationScreen} />
-//         <Stack.Screen name="LoginScreen" component={LoginScreen} />
-//         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   )
-// }
-// export default App;
-
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MainContainer from './screen/MainContainer';
